67c5800e9303b7d924538f0c7f7a9c42964fee2d
Márton Borsos <borsos.marton95@gmail.com>
2022-02-22 20:45:24 +0100

    Clearer value assignment

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -81,8 +81,8 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
 def fund_with_link(
     contract_address, account=None, link_token=None, amount=100000000000000000
 ):  # 0.1 LINK
-    account = account if account else get_account()
-    link_token = link_token if link_token else get_contract("link_token")
+    account = account or get_account()
+    link_token = link_token or get_contract("link_token")
     tx = link_token.transfer(contract_address, amount, {"from": account})
     # link_token_contract = interface.LinkTokenInterface(link_token.address)
     # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
